f4332636c3a70d6301aa45a4416baf06138f0041
Gourav-21 <rahul.dora21@gmail.com>
2025-05-02 15:26:30 +0530

    separated the lawyer modal

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import { Locale } from "../../i18n-config"; // Import Locale type
 import { getDictionary } from "../../get-dictionary"; // Import dictionary loader
 import FileAnalysis from "@/components/FileAnalysis";
+import LawyerModal from "@/components/LawyerModal";
 
 // Define props type to include lang
 interface HomeProps {
@@ -156,47 +157,8 @@ export default async function Home(props: HomeProps) {
           </div>
         </section>
 
-          {/* Modal */}
-          <div className="modal fade" id="exampleModal" tabIndex={-1} aria-labelledby="exampleModalLabel" aria-hidden="true">
-                <div className="modal-dialog modal-lg modal-dialog-centered">
-                    <div className="modal-content">
-                        <div className="modal-header">
-                            <h1 className="modal-title fs-5" id="exampleModalLabel">{dictionary.lawyerModal.title}</h1>
-                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"><i className="bi bi-x"></i></button>
-                        </div>
-                        <div className="modal-body p-3 p-sm-4">
-                            {dictionary.lawyerModal.lawyers.map((lawyer, index) => (
-                                <div key={index} className={`lawyer-card${index < dictionary.lawyerModal.lawyers.length - 1 ? ' mb-3' : ''}`}>
-                                    <img src={`img/lawyer-pic${index + 1}.jpg`} className="lawyer-profile me-3" alt={lawyer.name} />
-                                    <div className="lawyer-info">
-                                        <h4 className="mt-2">{lawyer.name}</h4>
-                                        <div className="row">
-                                            <div className="col-lg-6">
-                                                <ul className="text-sm lawyer-info-list">
-                                                    <li><i className="bi bi-geo-alt"></i> {lawyer.location}</li>
-                                                    <li><i className="bi bi-bullseye"></i> {lawyer.experience}</li>
-                                                    <li><i className="bi bi-star"></i> {lawyer.rating}</li>
-                                                </ul>
-                                            </div>
-                                            <div className="col-lg-6">
-                                                <h6>{lawyer.specialtiesTitle}</h6>
-                                                <ul className="text-sm">
-                                                    {lawyer.specialties.map((specialty, specialtyIndex) => (
-                                                        <li key={specialtyIndex}>{specialty}</li>
-                                                    ))}
-                                                </ul>
-                                            </div>
-                                        </div>
-                                        <button type="submit" className="btn btn-sm btn-outline-primary without-icon">
-                                            {lawyer.contactButton}
-                                        </button>
-                                    </div>
-                                </div>
-                            ))}
-                        </div>
-                    </div>
-                </div>
-            </div>
+        {/* Lawyer Modal */}
+        <LawyerModal lang={lang} dictionary={dictionary} />
       </main>
     </div>
   );

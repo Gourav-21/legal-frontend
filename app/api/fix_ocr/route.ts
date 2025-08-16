9ba848fd202a37b0c9710675847b9aae01a07256
Gourav-21 <rahul.dora21@gmail.com>
2025-08-16 17:38:02 +0530

    added api for ocr

---
+++ app/api/fix_ocr/route.ts
@@ -0,0 +1,10 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { proxyToBackend } from '../utils/proxyToBackend';
+
+export async function POST(request: NextRequest) {
+  const body = await request.json();
+  return proxyToBackend(request, '/api/fix_ocr', { 
+    method: 'POST', 
+    body: body 
+  });
+}
